6bcd107f08d576a14e712af4786b15649a7d56b4
wangqi <1015350043@qq.com>
2021-02-24 22:37:26 +0800

    remove extra code

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
-import { terser } from 'rollup-plugin-terser'
 import resolve from 'rollup-plugin-node-resolve'
 import postcss from 'rollup-plugin-postcss'
-// import { eslint } from 'rollup-plugin-eslint'
-import pkg from './package.json'
-
-const production = !process.env.ROLLUP_WATCH
 
 export default {
   input: "src/Loading.jsx",
@@ -36,6 +31,5 @@ export default {
     }),
     commonjs(),
     resolve(),
-    // terser(),
   ],
 }
