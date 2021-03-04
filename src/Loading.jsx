41ca88f7471fc10b39dded83ff7043aaf3d1637f
wangqi <1015350043@qq.com>
2021-03-04 15:04:06 +0800

    rename

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -3,7 +3,7 @@ import Loading1 from "./svg/loading1.jsx";
 import Loading2 from "./svg/loading2.jsx";
 import Loading3 from "./svg/loading3.jsx";
 import Loading4 from "./svg/loading4.jsx";
-import Loading5 from "./svg/loading1.jsx";
+import Loading5 from "./svg/loading5.jsx";
 import Loading6 from "./svg/loading6.jsx";
 import Loading7 from "./svg/loading7.jsx";
 import Loading8 from "./svg/loading8.jsx";
@@ -12,14 +12,14 @@ import "./loading.css";
 
 const Loading = ({ type, width, height, color }) => {
   const ldObj = {
-    1: <Loading1 width={width} height={height} color={color} />,
-    2: <Loading2 width={width} height={height} color={color} />,
-    3: <Loading3 width={width} height={height} color={color} />,
-    4: <Loading4 width={width} height={height} color={color} />,
-    5: <Loading5 width={width} height={height} color={color} />,
-    6: <Loading6 width={width} height={height} color={color} />,
-    7: <Loading7 width={width} height={height} color={color} />,
-    8: <Loading8 width={width} height={height} color={color} />,
+    Oval: <Loading1 width={width} height={height} color={color} />,
+    LittleCircle: <Loading2 width={width} height={height} color={color} />,
+    HalfCircle: <Loading3 width={width} height={height} color={color} />,
+    Audio: <Loading4 width={width} height={height} color={color} />,
+    Rain: <Loading5 width={width} height={height} color={color} />,
+    Stretch: <Loading6 width={width} height={height} color={color} />,
+    Flicker: <Loading7 width={width} height={height} color={color} />,
+    StretchFlicker: <Loading8 width={width} height={height} color={color} />,
   };
   return (
     <div>
