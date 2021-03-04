47dd5d5a15648ac00f8d7b38ba40e35d94ea5cb4
wangqi <1015350043@qq.com>
2021-03-04 22:34:28 +0800

    1

--- dist/loading.es.js
+++ dist/loading.es.js
@@ -50,8 +50,6 @@ styleInject(css_248z);
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-var loadingNames = ["Audio", "Oval", "Flicker", "HalfCircle", "LittleCircle", "Rain", "Stretch", "StretchFlicker"];
-loadingNames.includes(type) ? Spinner[type] : Spinner.Oval;
 var Loading = function Loading(props) {
   return /*#__PURE__*/React.createElement("div", {
     className: "porps."
