92e6335fd497eecbc9d18cad5a9f4af42b1c1e69
wangqi <1015350043@qq.com>
2021-03-04 23:12:38 +0800

    1

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./loading.css";
+import { Spinner } from "./svg/index";
 
 const loadingNames = [
   "Audio",
---
+++ src/svg/index.js
@@ -0,0 +1,19 @@
+import { Audio } from "./Audio.jsx";
+import { Oval } from "./Oval.jsx";
+import { Flicker } from "./Flicker.jsx";
+import { HalfCircle } from "./HalfCircle.jsx";
+import { LittleCircle } from "./LittleCircle.jsx";
+import { Rain } from "./Rain.jsx";
+import { Stretch } from "./Stretch.jsx";
+import { StretchFlicker } from "./StretchFlicker.jsx";
+
+export const Spinner = {
+  Audio,
+  Oval,
+  Flicker,
+  HalfCircle,
+  LittleCircle,
+  Rain,
+  Stretch,
+  StretchFlicker,
+};
